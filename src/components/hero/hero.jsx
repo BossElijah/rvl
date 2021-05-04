8dc087604e94cd33ba69c4804da2bc9a058e10d7
Elias Bruhn <elias@lfpost.dk>
2021-05-04 14:57:20 +0200

    Create Hero component

---
+++ src/components/hero/hero.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Hero = () => (
+  <div className="hero">
+    <img src="https://editor.revealit.dk/sites/default/files/2019-08/reveal-logo-light-blue.svg" alt="Logo with text"/>
+    <h1 className="hero__title">Drupal + React Specialister</h1>
+  </div>
+)
+
+export default Hero;
