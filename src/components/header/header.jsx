50f5caa3c4bee6201773500e74c617795dedd9d5
Elias Bruhn <elias@lfpost.dk>
2021-05-04 14:56:59 +0200

    Give header 'position: absolute;' in order to let Hero slide up under

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import Logo from '../logo/logo';
 
-const Header = () => {
-  return (
-    <div className="header">
-      <a href="/">
-        <Logo />
-      </a>
-      <div className="header__language-container">
-        <a href="/en">EN</a>
-        <a href="/">DA</a>
-      </div>
+const Header = () => (
+  <div className="header">
+    <a href="/">
+      <Logo />
+    </a>
+    <div className="header__language-container">
+      <a href="/en">EN</a>
+      <a href="/">DA</a>
     </div>
-  );
-};
+  </div>
+);
 
 export default Header;
