d53723b3fc27fba3acb4e5d2ee1b07fc5c52135f
Elias Bruhn <elias@lfpost.dk>
2021-05-04 14:55:39 +0200

    Create Main component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ScrollToTop from './ScrollToTop';
 import Header from './components/header/header';
+import Main from './Main';
 import Footer from './components/footer/footer';
 import './styles/styles.scss';
 
-const App = () => {
-  return (
-    <Router>
-      <ScrollToTop />
-      <Header />
-      {/* <Route component={component} /> */}
-      <Footer />
-    </Router>
-  );
-};
+const App = () => (
+  <Router>
+    <ScrollToTop />
+    <Header />
+    <Route component={Main} />
+    <Footer />
+  </Router>
+);
 
 export default App;
---
+++ src/Main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Hero from './components/hero/hero';
+
+const Main = () => <Hero />;
+
+export default Main;
