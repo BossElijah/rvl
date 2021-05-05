76a992d0b9600dec9d1378311549e52014429cb0
Elias Bruhn <elias@lfpost.dk>
2021-05-05 14:43:46 +0200

    Make hover work only on hover devices

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Hero from './components/hero/hero';
 import TeaserDeck from './components/teaser-deck/teaser-deck';
 import SectionTitle from './components/section-title/section-title';
-import BulletList from './components/bullet-list/bullet-list';
 import Wysiwyg from './components/wysiwyg/wysiwyg';
 import Banner from './components/banner/banner';
 import TeaserList from './components/teaser-list/teaser-list';
@@ -13,13 +12,11 @@ const Main = () => (
     <Hero />
     <TeaserDeck />
     <TeaserDeck />
-    <SectionTitle />
-    <BulletList />
     <Wysiwyg />
     <Banner />
     <SectionTitle />
     <TeaserList />
-    <Wysiwyg noteForLater="This Wysiwyg has to have some completely other content." />
+    <Wysiwyg />
     <CardList />
   </>
 );
