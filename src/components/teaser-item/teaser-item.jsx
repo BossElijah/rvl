7f056e845028baa50cd810560e294bcfa195be7e
Elias Bruhn <elias@lfpost.dk>
2021-05-05 09:58:04 +0200

    Create and style teaser-list-, and teaser-item components

---
+++ src/components/teaser-item/teaser-item.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TeaserItem = ({ icon, title, text }) => (
+  <div className="teaser-item">
+    <div className="teaser-item__title-container">
+      {icon} <h3 className="teaser-item__title">{title}</h3>
+    </div>
+    <p className="teaser-item__text">{text}</p>
+  </div>
+);
+
+export default TeaserItem;
