4ff969800fc0f831388684c3e633ccefdd62cae2
Elias Bruhn <elias@lfpost.dk>
2021-05-05 14:42:50 +0200

    Make teaser-deck and and card-list responsive

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -6,7 +6,6 @@ const Card = () => (
       src="https://editor.revealit.dk/sites/default/files/styles/scale_max_2560w/public/2019-09/businessman_drawing_a_graph.jpg?itok=Wxcb6dPp"
       alt="This is a card IMG"
     />
-
     <div className="card__content">
       <h3 className="card__title">Online-booking til hoteller</h3>
       <p className="card__text">
