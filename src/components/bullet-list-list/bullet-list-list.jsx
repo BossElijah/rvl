f425801942df64c2bb6708125affba2d415accd2
Elias Bruhn <elias@lfpost.dk>
2021-05-07 14:19:04 +0200

    Add container for bullet lists

--- src/components/bullet-list-list/bullet-list-list.jsx
+++ src/components/bullet-list-list/bullet-list-list.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import BulletList from '../bullet-list/bullet-list';
 
 const BulletListList = ({ list }) => {
-  const finalData = list.map((item, index) => (
-    <BulletList key={index} {...item} />
-  ));
-  return finalData;
+  let counter = 0;
+  const finalData = list.map((item, index) => {
+    counter++;
+    return <BulletList key={index} {...item} />;
+  });
+  return (
+    <div
+      className={`bullet-list-container${
+        counter > 1 ? ' bullet-list-container-2-col' : ''
+      }`}
+    >
+      {finalData}
+    </div>
+  );
 };
 
 export default BulletListList;
