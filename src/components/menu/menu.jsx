d16a08349cd3273f41d52bb375815433b7f5b7a9
Elias Bruhn <elias@lfpost.dk>
2021-05-07 14:20:55 +0200

    Remove unused curly brackets

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 
-const Menu = () => {
-  return (
-    <div className="menu">
-      <a href="/">Forside</a>
-      <a href="/jeres-udvidede-web-afdeling">Konsulenter</a>
-      <a href="/drupal">Drupal</a>
-      <a href="/react">React</a>
-      <a href="/online-booking-system-hotel">Hotel-booking</a>
-      <a href="/artikler">Artikler</a>
-      <a href="/om-os">Om os</a>
-    </div>
-  );
-};
+const Menu = () => (
+  <div className="menu">
+    <a href="/">Forside</a>
+    <a href="/jeres-udvidede-web-afdeling">Konsulenter</a>
+    <a href="/drupal">Drupal</a>
+    <a href="/react">React</a>
+    <a href="/online-booking-system-hotel">Hotel-booking</a>
+    <a href="/artikler">Artikler</a>
+    <a href="/om-os">Om os</a>
+  </div>
+);
 
 export default Menu;
