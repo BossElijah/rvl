3730627b1e6bd14d8d02d4d7ca551c4bf045fd69
Elias Bruhn <elias@lfpost.dk>
2021-05-10 18:20:16 +0200

    Change hero background

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 const Hero = ({ title, logo, backgroundImg }) => (
   <div className="hero">
-    <div className="hero-filter">
-      <img src={backgroundImg} alt="Hero" className="hero__background-image" />
-    </div>
+    <div
+      className="hero__background-image"
+      style={{ backgroundImage: `url(${backgroundImg})` }}
+    />
     {logo}
     {title && <h1 className="hero__title">{title}</h1>}
   </div>
