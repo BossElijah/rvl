7bb72ab66f90e248d206a1d2b05291e735580bde
Elias Bruhn <elias@lfpost.dk>
2021-05-10 13:29:35 +0200

    Add buton to contact-form

--- src/components/contact-form/contact-form.jsx
+++ src/components/contact-form/contact-form.jsx
@@ -3,9 +3,10 @@ import InputField from '../input-field/input-field';
 
 const ContactForm = () => (
     <div className="contact-form">
-      <InputField type="name" placeholder="Dit fulde navn..." require />
-      <InputField type="email" placeholder="Din email adresse..." require />
-      <InputField type="text" placeholder="Din besked..." require />
+      <InputField type="input" name="name" placeholder="Dit fulde navn..." require />
+      <InputField type="input" name="email" placeholder="Din email adresse..." require />
+      <InputField type="textarea" name="text" placeholder="Din besked..." require />
+      <button type="button" className="button button--outline">Send besked</button>
     </div>
   )
 
--- src/components/input-field/input-field.jsx
+++ src/components/input-field/input-field.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const InputField = ({ type, placeholder, require }) => (
-  <input
-    type={type}
+const InputField = ({ type, name, placeholder, require }) => (
+  <>
+  {type === "input" && <input
+    type={name}
     placeholder={`${placeholder}${require ? '  *' : ''}`}
-    className={`input-field input-field--${type}`}
-  />
+    className={`input-field input-field--${name}`}
+  />}
+  {type === "textarea" && <textarea
+    type={name}
+    placeholder={`${placeholder}${require ? '  *' : ''}`}
+    className={`input-field input-field--${name}`}
+  />}
+  </>
 );
 
 export default InputField;
