9a00fe81044466732e0b4a319e5f4e7a6769ab12
Elias Bruhn <elias@lfpost.dk>
2021-05-17 17:53:31 +0200

    Pass array in through props correctly

--- src/components/bullet-list-container/bullet-list-container.jsx
+++ src/components/bullet-list-container/bullet-list-container.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import BulletList from '../bullet-list/bullet-list';
 
-const BulletListContainer = ({ list }) => {
-  let counter = 0;
-  const finalData = list.map((item, index) => {
-    counter++;
-    return <BulletList key={index} {...item} />;
-  });
-  return (
-    <div
-      className={`bullet-list-container${
-        counter > 1 ? ' bullet-list-container-2-col' : ''
-      }`}
-    >
-      {finalData}
-    </div>
-  );
-};
+const BulletListContainer = ({ list }) => (
+  <section
+    className={`bullet-list-container${
+      list.length > 1 ? ' bullet-list-container-2-col' : ''
+    }`}
+  >
+    {list.map((item, index) => {
+      return <BulletList key={index} item={item} />;
+    })}
+  </section>
+);
 
 export default BulletListContainer;
--- src/components/bullet-list/bullet-list.jsx
+++ src/components/bullet-list/bullet-list.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
 import BulletSVG from '../../images/bullet.svg';
 
-const BulletList = item => {
-  const result = Object.keys(item).map(key => [Number(key), item[key]]);
-
-  const finalData = result.map((listItem, index) => (
-    <li key={index} className="bullet-list__item">
-      <BulletSVG />
-      <span>{listItem[1].text}</span>
-    </li>
-  ));
-  return <ul className="bullet-list">{finalData}</ul>;
-};
+const BulletList = ({ item }) => (
+  <ul className="bullet-list">
+    {item.map((listItem, index) => (
+      <li key={index} className="bullet-list__item">
+        <BulletSVG />
+        <span>{listItem.text}</span>
+      </li>
+    ))}
+  </ul>
+);
 
 export default BulletList;
