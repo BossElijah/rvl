2390bc1611e04b7d2dff2ec6ede8261db4c47627
Elias Bruhn <elias@lfpost.dk>
2021-05-18 15:52:57 +0200

    Put contact form inside a <form> element

--- src/components/contact-form/contact-form.jsx
+++ src/components/contact-form/contact-form.jsx
@@ -5,24 +5,20 @@ import InputField from '../input-field/input-field';
 const ContactForm = ({ title, image, text }) => (
   <section className="contact-form">
     <TextAndMedia title={title} image={image} text={text} />
-    <InputField
-      name="name"
-      placeholder="Dit fulde navn..."
-      require
-    />
-    <InputField
-      name="email"
-      placeholder="Din email adresse..."
-      require
-    />
-    <textarea
-      name="text"
-      className="input-field"
-      placeholder="Din besked...  *"
-    />
-    <button type="submit" className="button button--outline">
-      Send besked
-    </button>
+    <form>
+      <InputField name="name" placeholder="Dit fulde navn..." require />
+      <InputField name="email" placeholder="Din email adresse..." require />
+      <textarea
+        name="text"
+        className="input-field"
+        placeholder="Din besked...  *"
+      />
+      <input
+        type="submit"
+        className="button button--outline"
+        value="Send Besked"
+      />
+    </form>
   </section>
 );
 
