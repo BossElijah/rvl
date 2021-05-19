9a8dae744a893969e08db5d94b37f05b8a879ab7
Elias Bruhn <elias@lfpost.dk>
2021-05-19 16:41:16 +0200

    Fix tabIndex console error

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -4,49 +4,49 @@ import { Link } from 'react-router-dom';
 const Menu = ({ showMenu, setShowMenu }) => (
   <div className={`menu${showMenu ? ' menu--open' : ''}`}>
     <Link
-      tabIndex={!showMenu && '-1'}
+      tabIndex={!showMenu ? -1 : 0}
       onClick={() => setShowMenu(!showMenu)}
       to="/"
     >
       Forside
     </Link>
     <Link
-      tabIndex={!showMenu && '-1'}
+      tabIndex={!showMenu ? -1 : 0}
       onClick={() => setShowMenu(!showMenu)}
       to="/jeres-udvidede-web-afdeling"
     >
       Konsulenter
     </Link>
     <Link
-      tabIndex={!showMenu && '-1'}
+      tabIndex={!showMenu ? -1 : 0}
       onClick={() => setShowMenu(!showMenu)}
       to="/drupal"
     >
       Drupal
     </Link>
     <Link
-      tabIndex={!showMenu && '-1'}
+      tabIndex={!showMenu ? -1 : 0}
       onClick={() => setShowMenu(!showMenu)}
       to="/react"
     >
       React
     </Link>
     <Link
-      tabIndex={!showMenu && '-1'}
+      tabIndex={!showMenu ? -1 : 0}
       onClick={() => setShowMenu(!showMenu)}
       to="/online-booking-system-hotel"
     >
       Hotel-booking
     </Link>
     <Link
-      tabIndex={!showMenu && '-1'}
+      tabIndex={!showMenu ? -1 : 0}
       onClick={() => setShowMenu(!showMenu)}
       to="/artikler"
     >
       Artikler
     </Link>
     <Link
-      tabIndex={!showMenu && '-1'}
+      tabIndex={!showMenu ? -1 : 0}
       onClick={() => setShowMenu(!showMenu)}
       to="/om-os"
     >
