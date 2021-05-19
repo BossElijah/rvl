e28cb6970d905c6bdcd3c8ef8d14234536af1a71
Elias Bruhn <elias@lfpost.dk>
2021-05-19 11:44:27 +0200

    Remove unused imports and create global.json

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,4 @@
 import React from 'react';
-
-// Components.
 import Hero from './components/hero/hero';
 import TeaserDeck from './components/teaser-deck/teaser-deck';
 import PageTitle from './components/page-title/page-title';
@@ -11,19 +9,6 @@ import Banner from './components/banner/banner';
 import TeaserList from './components/teaser-list/teaser-list';
 import CardList from './components/card-list/card-list';
 import ContactForm from './components/contact-form/contact-form';
-
-// SVGs.
-import DrupalSVG from './images/drupal-white.svg';
-import DrupalBigSVG from './images/drupal-big.svg';
-import ReactSVG from './images/react.svg';
-import ReactBigSVG from './images/react-big.svg';
-import SkillsSVG from './images/skills.svg';
-import BrainSVG from './images/brain.svg';
-import TelevizionSVG from './images/televizion.svg';
-import FolderSVG from './images/folder.svg';
-import RvlLogoTextSVG from './images/rvl-logo-text.svg';
-import DrupalThinkReactSVG from './images/drupal-think-react.svg';
-import ManSittingAtDesk from './images/man-sitting-desk.svg';
 
 const Deck = ({ type, ...props }) => {
   const deckMap = {
@@ -36,12 +21,11 @@ const Deck = ({ type, ...props }) => {
     banner: Banner,
     teaserList: TeaserList,
     cardList: CardList,
-    contactForm: ContactForm,
+    contactForm: ContactForm
   };
 
   return React.createElement(deckMap[type], props, null);
 };
-
 
 const pages = {
   '/': 'hjem.json',
@@ -54,7 +38,10 @@ const Main = ({ location }) => {
     const currentPage = require(`./data/${pages[location.pathname]}`);
     const pageTitle =
       pathname.charAt(0).toUpperCase() +
-      pathname.slice(1).substring(0, pathname.length - 6).replace(/-/g, ' ');
+      pathname
+        .slice(1)
+        .substring(0, pathname.length - 6)
+        .replace(/-/g, ' ');
     document.title = `${pageTitle} | Reveal IT`;
     return currentPage.decks.map((deck, index) => (
       <Deck key={index} type={deck.type} {...deck} />
