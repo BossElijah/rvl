19f90fb0d3f4aeff775ff8cad176b4e661988e56
Elias Bruhn <elias@lfpost.dk>
2021-05-19 11:38:14 +0200

    Edit tabindex to tabIndex

--- src/components/menu/menu.jsx
+++ src/components/menu/menu.jsx
@@ -4,49 +4,49 @@ import { Link } from 'react-router-dom';
 const Menu = ({ showMenu, setShowMenu }) => (
   <div className={`menu${showMenu ? ' menu--open' : ''}`}>
     <Link
-      tabindex={!showMenu && '-1'}
+      tabIndex={!showMenu && '-1'}
       onClick={() => setShowMenu(!showMenu)}
       to="/"
     >
       Forside
     </Link>
     <Link
-      tabindex={!showMenu && '-1'}
+      tabIndex={!showMenu && '-1'}
       onClick={() => setShowMenu(!showMenu)}
       to="/jeres-udvidede-web-afdeling"
     >
       Konsulenter
     </Link>
     <Link
-      tabindex={!showMenu && '-1'}
+      tabIndex={!showMenu && '-1'}
       onClick={() => setShowMenu(!showMenu)}
       to="/drupal"
     >
       Drupal
     </Link>
     <Link
-      tabindex={!showMenu && '-1'}
+      tabIndex={!showMenu && '-1'}
       onClick={() => setShowMenu(!showMenu)}
       to="/react"
     >
       React
     </Link>
     <Link
-      tabindex={!showMenu && '-1'}
+      tabIndex={!showMenu && '-1'}
       onClick={() => setShowMenu(!showMenu)}
       to="/online-booking-system-hotel"
     >
       Hotel-booking
     </Link>
     <Link
-      tabindex={!showMenu && '-1'}
+      tabIndex={!showMenu && '-1'}
       onClick={() => setShowMenu(!showMenu)}
       to="/artikler"
     >
       Artikler
     </Link>
     <Link
-      tabindex={!showMenu && '-1'}
+      tabIndex={!showMenu && '-1'}
       onClick={() => setShowMenu(!showMenu)}
       to="/om-os"
     >
