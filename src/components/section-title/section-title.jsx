2447ef9880dcb1a8965a0f8b5384c1d37c6bb6e6
Elias Bruhn <elias@lfpost.dk>
2021-05-30 10:52:30 +0200

    Create hotels page

--- src/components/section-title/section-title.jsx
+++ src/components/section-title/section-title.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const SectionTitle = ({ title, className }) => <h2 className={className ? `section-title ${className}` : 'section-title'}>{title}</h2>;
+const SectionTitle = ({ title, className }) => <h2 className={className ? `section-title section-title--${className}` : 'section-title'}>{title}</h2>;
 
 export default SectionTitle;
--- src/components/text-and-media/text-and-media.jsx
+++ src/components/text-and-media/text-and-media.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import Svg from '../svg/svg';
 import SectionTitle from '../section-title/section-title';
 
-const TextAndMedia = ({ title, image, text }) => (
+const TextAndMedia = ({ title, image, imageType, text }) => (
   <section className="text-and-media">
     <SectionTitle title={title} />
     <div className="text-and-media__content">
-      <Svg type={image} />
+    {imageType ? <img src={image} alt="" /> : <Svg type={image} />}
       <p dangerouslySetInnerHTML={{ __html: text }} />
     </div>
   </section>
